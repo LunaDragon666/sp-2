b64d8f0d730e754582ecf54b827043285268b16a
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-03-05 21:23:04 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,49 @@
+import { baseUrl } from "./settings/api.js";
+import createMenu from "./components/common/createMenu.js";
+import displayMessage from "./components/common/displayMessage.js";
+import searchProducts from "./ui/searchProducts.js";
+
+createMenu();
+
+async function fetchProducts () {
+    const productsUrl = baseUrl + "/makeups";
+    try {
+        const response = await fetch(productsUrl);
+        const json = await response.json();
+        productsContainer(json);
+        searchProducts(json);
+}catch{
+  console.log (error)
+          displayMessage("error", error, ".row");
+}
+};
+
+fetchProducts();
+
+export function productsContainer (json){
+  const container = document.querySelector(".row");
+        container.innerHTML = "";
+
+        json.forEach(function (product) {
+          //console.log(product);
+            container.innerHTML += `
+            <div class="col-md-3">
+            <a class="product" href="detail.html?id=${product.id}" style="text-decoration: none">
+            <div class="card-block h-100" style="width: 15rem;">
+            <div class="card-body">
+            <h2 class="card-title" style="font-size: 1.25rem;" data-toggle="tooltip" data-placement="bottom" title="${product.brand} ${product.name} ${product.color}">
+                ${product.brand}
+                ${product.name}
+                ${product.color}
+             </h2>
+             <p class="card-text">$ ${product.price}</p>
+             </a>
+             <button class="cart-btn" data-product="${product.id}">Add to cart</button>
+             <a class="edit-btn" href="edit.html?id=${product.id}">Edit</a>
+             </div>
+             </div>
+             </div>
+             `
+             ;
+        });
+};
