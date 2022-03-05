94ece69880c1a090b8167a41137739b7492db372
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-03-05 21:22:52 +0100

    Create deleteButton.js

---
+++ js/components/products/deleteButton.js
@@ -0,0 +1,41 @@
+import { baseUrl } from "../../settings/api.js";
+import { getToken } from "../../utils/storage.js";
+
+export default function deleteButton(id) {
+    const deleteContainer = document.querySelector(".delete-arena");
+
+    deleteContainer.innerHTML = `<button type="button class="delete>Delete</button>`;
+
+    const deleteBtn = document.querySelector("button.delete");
+
+    deleteBtn.onclick = async function () {
+        console.log(id);
+
+        const doDelete = confirm("Are you sure?");
+        console.log(doDelete);
+
+        if(doDelete) {
+            const url = baseUrl = "/makeups/" + id;
+
+            const token = getToken();
+    
+            const options = {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+    
+            try {
+                const response = await fetch(url, options);
+                const json = await response.json();
+
+                location.href = "products.html";
+    
+                console.log(json);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+}
